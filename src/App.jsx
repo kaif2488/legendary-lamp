b868f4ab57a6447749f3865b774ad8b592f3d05f
Kaif Siddique <kaifsiddique2488@gmail.com>
2026-01-29 00:41:27 +0530

    Major Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import { property } from './components/property'
+import './App.css';
+import FlatDetail from "./components/FlatDetail.jsx"
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+import Home from "./components/Home.jsx"
+import Contact from "./components/Contact.jsx"
+import About from "./components/About.jsx"
+import Blog from "./components/Blog.jsx"
+import BlogDetail from "./components/BlogDetail.jsx"
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <><p>
-    <property></property></p>
-    </>
-  )
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
+        <Route path="/flat/:slug" element={<FlatDetail />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
